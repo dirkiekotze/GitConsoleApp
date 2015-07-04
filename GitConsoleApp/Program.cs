3ff3d3e9c2d48ebe557c35b73629994fafd8e1cd
Dirk <dirkkotze@cvglobal.co>
2015-07-04 14:08:41 +1000

    I am master Change

--- GitConsoleApp/Program.cs
+++ GitConsoleApp/Program.cs
@@ -4,21 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace GitConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string name = "I am the MasterBranch";
-            SomeFunctionInError(name);
-            
+            //I am master;            
         }
 
-        private static void SomeFunctionInError(string name)
-        {
-            //You better do something here;
-            int val = 1 = 1;
-        }
+        
     }
 }
